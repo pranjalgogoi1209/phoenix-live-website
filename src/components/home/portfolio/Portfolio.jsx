16e9b9c2a53da2b07436bd539a1cac15c23ea153
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-03-01 19:37:55 +0530

    update

--- src/components/home/portfolio/Portfolio.jsx
+++ src/components/home/portfolio/Portfolio.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "./portfolio.module.css";
 import { portfolio } from "../../../data/home/portfolio";
-import riders from "./../../../assets/projects/riders-music-festival.jpg";
 
 export default function Portfolio() {
   return (
@@ -9,29 +8,27 @@ export default function Portfolio() {
       <h1>
         OUR <span>PROJECTS</span>
       </h1>
-      <div className={styles.leftContainer}>
-        <div className={styles.imgContainer}>
-          <img src={riders} alt="riders" />
-        </div>
-        <div className={styles.title}>
-          <strong>RIDERS MUSIC FESTIVAL</strong>
-        </div>
+
+      <div className={styles.eventContainer}>
+        {portfolio?.map((item, index) => (
+          <div className={styles.parentEvent}>
+            {item?.map((item2, index2) => (
+              <div className={styles[item2.class]} key={index2}>
+                {console.log("item2", item2)}
+                <div className={styles.imgContainer}>
+                  <img src={item2.img} alt="event" />
+                </div>
+                <div className={styles.content}>
+                  <h2>{item2.title}</h2>
+                  <p>description...</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
 
-      <div className={styles.rightContainer}>
-        <div className={styles.imgParent}>
-          {portfolio?.map((item, index) => (
-            <div>
-              <div className={styles.imgContainer}>
-                <img src={item.img} alt="project" />
-              </div>
-              <div className={styles.title}>
-                <strong>{item.title.toUpperCase()}</strong>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+      <button>SEE ALL</button>
     </div>
   );
 }
--- src/data/home/portfolio.js
+++ src/data/home/portfolio.js
@@ -1,35 +1,31 @@
 import microsoft from "./../../assets/projects/microsoft-e-d.png";
 import google from "./../../assets/projects/google-holiday-party-aftermovies.png";
 import visa from "./../../assets/projects/visa-annual-party.png";
-import explore from "./../../assets/projects/explore-more.jpg";
+import riders from "./../../assets/projects/riders-music-festival.png";
 
 export const portfolio = [
-  {
-    title: "Microsoft E+D",
-    img: microsoft,
-    color: "yellow",
-    bg: "#251e1c",
-    class: "vertical",
-  },
-  {
-    title: "Google Holiday Party",
-    img: google,
-    color: "orange",
-    bg: "#222935",
-    class: "horizontal",
-  },
-  {
-    title: "Visa Annual Party",
-    img: visa,
-    color: "orange",
-    bg: "#220c04",
-    class: "horizontal",
-  },
-  {
-    title: "EXPLORE MORE",
-    img: explore,
-    color: "purple",
-    bg: "#222935",
-    class: "vertical",
-  },
+  [
+    {
+      title: "Microsoft E+D",
+      img: microsoft,
+      class: "top",
+    },
+    {
+      title: "Visa Annual Party",
+      img: visa,
+      class: "bottom",
+    },
+  ],
+  [
+    {
+      title: "Riders Music Festival",
+      img: riders,
+      class: "top",
+    },
+    {
+      title: "Google Holiday Party",
+      img: google,
+      class: "bottom",
+    },
+  ],
 ];
